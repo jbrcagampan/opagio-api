3d3c1490739b07a55c27dce9a39eed814b8fb9df
Arthur Henderson <arthur@techpassport.io>
2023-02-22 23:31:11 +0800

    db connect stats

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var cors = require("cors");
 require("./models/leads/leads.model.schema");
 var config = require("./config/config");
 
-mongoose.connect(config.mongodb, { useNewUrlParser: true });
+mongoose.connect(config.mongodb, { useNewUrlParser: true }).then(() => {
+  console.log('Connected to DB!')
+})
 var app = express();
 app.use(cors());
 app.use(require("./models/leads/leads.router"));
